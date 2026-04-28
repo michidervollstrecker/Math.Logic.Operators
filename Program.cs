3f031fbbf771e772bbbffe8577b3e1f1e2a05e30
michi <bergen.michael@web.de>
2026-04-28 14:46:09 +0200

    a

--- Program.cs
+++ Program.cs
@@ -17,28 +17,6 @@
                 }
                 _i16min++;
             }
-
-            if (a == 13 && b == 13)
-            {
-                Console.WriteLine(" a ist 13 und b ist 13");
-            } // Wenn a && UND b 13 ist
-
-            // Jetzt kommt null und ??
-            // Nochmal zum Anfang a hat den Wert 3 zugewiesen bekommen und nimmt somit Platz im Arbeitsspeicher ein.
-            // NULL heißt dass der Platz im Speicher nicht beschrieben ist
-            // Angenommen du fragst ein Textfeld ab und dieses Textfeld geht davon aus, dass der Nutzer eine Eingabe machen wird,
-            // somit hat das Textfeld bereits einen Platz im Arbeitsspeicher reserviert.
-            // NULL bedeutet dass kein Platz im Arbeitsspeicher reserviert ist.
-            // Das ?? Bedeutet, dass wenn der Nutzer ein Textfeld ausfüllen soll 
-            // und der Nutzer hat aber keinen Wert eingetragen, wird es aber trotzdem nicht zu einem Error kommen, 
-            // da die Variable vom UserInput ?? ist somit auch wahr ist wenn der Nutzer keinen Wert eingibt.
-
-
-            string n = null; // n wird hier als string deklariert und hat den Wert null ist also Empty.
-            string up = Console.ReadLine();
-            string? up2 = up + up;      // Das ? macht, dass es keinen Fehler ausgibt, wenn der Wert nicht korrekt zugewiesen ist.
-            Console.WriteLine(up2);
-
         }
     }
 }
